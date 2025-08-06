a5a9a82f16aab9186530936463b15c21d97fd3d9
ChiBao <baotochi87@gmail.com>
2025-08-06 09:32:15 +0700

    notes about string

---
+++ app.py
@@ -0,0 +1,39 @@
+# def main():
+#     i = 1
+#     max = 10
+#     while(i <= max):
+#         print(i)
+#         i += 1
+# main()
+# # This is comment
+
+import keyword
+
+# print(keyword.kwlist)
+
+
+# ! String 
+# s1 = 'String literal 1'
+# s2 = "String literal 2"
+# s3 = '''String literal 3'''
+
+# print(s1)
+# print(s2)
+# print(s3)
+
+# name = "Chi Bao"
+# greetingMsg = f'Hello {name}'
+
+# print(greetingMsg)
+
+# greeting = "Hello " "Python"
+# print(greeting[:5])
+
+# ? String is immutable
+# str = "Chi Bao"
+# str[1] = "A"
+
+# print(str)
+
+
+# ! Number
