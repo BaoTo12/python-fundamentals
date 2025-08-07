175c286e3f15a19b074cc7b5f3a043e6051c2ebb
ChiBao <baotochi87@gmail.com>
2025-08-07 08:47:50 +0700

    class & module

--- class.py
+++ class.py
@@ -48,22 +48,69 @@
 
 
 #! Inheritance
-class Person:
-    def __init__(self, fname, lname):
-        self.firstname = fname
-        self.lastname = lname
+# class Person:
+#     def __init__(self, fname, lname):
+#         self.firstname = fname
+#         self.lastname = lname
 
-    def printname(self):
-        print(self.firstname, self.lastname)
+#     def printname(self):
+#         print(self.firstname, self.lastname)
 
 # Use the Person class to create an object, and then execute the printname method:
 
 
-class Student(Person):
-    def __init__(self, fname, lname):
-        super().__init__( fname, lname)
+# class Student(Person):
+#     def __init__(self, fname, lname):
+#         super().__init__( fname, lname)
 
 
-s1 = Student("Thang", 24)
+# s1 = Student("Thang", 24)
 
-s1.printname()
+# s1.printname()
+
+
+#! Polymorphism
+
+# class Vehicle:
+#     def __init__(self, brand, model):
+#         self.brand = brand
+#         self.model = model
+
+#     def move(self):
+#         print("Move!")
+
+
+# class Car(Vehicle):
+#     pass
+
+
+# class Boat(Vehicle):
+#     def move(self):
+#         print("Sail!")
+
+
+# class Plane(Vehicle):
+#     def move(self):
+#         print("Fly!")
+
+
+# car1 = Car("Ford", "Mustang")  # Create a Car object
+# boat1 = Boat("Ibiza", "Touring 20")  # Create a Boat object
+# plane1 = Plane("Boeing", "747")  # Create a Plane object
+
+# for x in (car1, boat1, plane1):
+#     print(x.brand, x.model)
+#     x.move()
+
+
+import mymodule as mx
+import platform
+
+# mx.greeting("Jonathan")
+
+# print(mx.person1)
+
+print(platform.system())
+
+x = dir(platform)
+print(x)
---
+++ iterator.py
@@ -0,0 +1,40 @@
+
+#! List is iterable
+# myList = ["Bao", "Thang", "Nam"]
+
+# iterator = iter(myList)
+
+# for item in iterator.__iter__():
+#     print(item)
+
+#! String is iterable object
+# mystr = "banana"
+# myit = iter(mystr)
+
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+
+
+# class MyNumbers:
+#     def __iter__(self):
+#         self.a = 1
+#         return self
+
+#     def __next__(self):
+#         x = self.a
+#         self.a += 1
+#         return x
+
+
+# myclass = MyNumbers()
+# myiter = iter(myclass)
+
+# print(next(myiter))
+# print(next(myiter))
+# print(next(myiter))
+# print(next(myiter))
+# print(next(myiter))
